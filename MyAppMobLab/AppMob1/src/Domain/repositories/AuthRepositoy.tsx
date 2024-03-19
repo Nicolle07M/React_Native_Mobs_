da14709126ee22b535350a3df5aead06e4a9cd10
Nicolle07M <marqueznicolle421@gmail.com>
2024-03-19 17:16:49 -0500

    feat(fronend): Add domain and data, folders and documents

---
+++ MyAppMobLab/AppMob1/src/Domain/repositories/AuthRepositoy.tsx
@@ -0,0 +1,4 @@
+import { User } from "../entities/User";
+export interface AuthRepository {
+ register(user: User): Promise<any>;
+}
--- MyAppMobLab/AppMob1/src/Presentation/views/register/viewModel.tsx
+++ MyAppMobLab/AppMob1/src/Presentation/views/register/viewModel.tsx
@@ -1,32 +1,29 @@
-import React, { useState } from 'react'
-import { ApiDelivery } from '../../../Data/sources/remote/api/ApiDelivery'; 
-
-
+import React, { useState } from "react"
+import { ApiDelivery } from "../../../Data/sources/remote/api/ApiDelivery";
+import { RegisterAuthUseCase } from "../../../Domain/useCases/auth/RegisterAuth";
 const RegisterViewModel = () => {
-    const [values, setValues] = useState({
-    name: '',
-    lastname: '',
-    phone: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
- });
-    const onChange = (property: string, value: any) => {
-    setValues({ ...values, [property]: value });
- }
- const register = async () => {
-   try {
-   const response = await ApiDelivery.post('/users/create', values);
-   console.log('RESPONSE: ' + JSON.stringify(response));
-  
-   } catch (error) {
-   console.log('ERROR: ' + error);
-   }
+   const [values, setValues] = useState({
+      name: '',
+      lastname: '',
+      email: '',
+      phone: '',
+      password: '',
+      confirmPassword: ''
+   });
+ const onChange = (property: string, value: any) => {
+      setValues({ ...values, [property]: value });
    } 
+ const register = async() => {
+      const { result, error } = await RegisterAuthUseCase(values);
+      console.log('result' + JSON.stringify(result));
+      console.log('error' + error);
+   }
+
  return {
-    ...values,
-    onChange,
-    register
- }
+      ...values,
+      onChange,
+      register
+   }
 }
+
 export default RegisterViewModel; 
