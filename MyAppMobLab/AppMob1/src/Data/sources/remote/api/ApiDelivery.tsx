4d19bcbf2c8e28fb49630a9642b7ea382144d43f
Nicolle07M <marqueznicolle421@gmail.com>
2024-03-19 16:11:01 -0500

    feat(frontend): add data

--- MyAppMobLab/AppMob1/src/Data/sources/remote/api/ApiDelivery.tsx
+++ MyAppMobLab/AppMob1/src/Data/sources/remote/api/ApiDelivery.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const ApiDelivery = axios.create({
-    baseURL: 'https://192.168.20.174:3202/api',
+    baseURL: 'http://192.168.20.174:3202/api',
     headers: {
         'Content-Type': 'application/json'
     }
